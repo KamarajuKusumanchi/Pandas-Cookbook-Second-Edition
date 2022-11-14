248037054eb678a7eea1cdf58f36fed6e7fd1f1a
Kamaraju S. Kusumanchi <kamaraju@gmail.com>
2022-11-14 15:50:06 -0500

    lgtm

---
+++ raju/pg-xiii.py
@@ -0,0 +1,22 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.14.1
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+import pandas as pd
+employee = pd.read_csv('../data/employee.csv')
+employee
+
+employee.groupby(['DEPARTMENT'])['BASE_SALARY'].max()
+
+
